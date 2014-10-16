02cddbd5680e35b1df32d0bf3ec76e4b77f3ecb5
Troy Mass <masstroy21@gmail.com>
2014-10-16 01:19:32 -0500

    Not quite working walls to interior heat transfer

--- WebApplication/src/main/java/iit/ipro307/domain/SegmentTemperatureCalculator.java
+++ WebApplication/src/main/java/iit/ipro307/domain/SegmentTemperatureCalculator.java
@@ -1,6 +1,7 @@
 package iit.ipro307.domain;
 
 import iit.ipro307.domain.data.Temperature;
+import iit.ipro307.domain.data.TemperatureUnit;
 import iit.ipro307.domain.data.WeatherData;
 
 import java.util.Date;
@@ -13,22 +14,17 @@ import common.util.PrefixLogger;
 
 @Service
 public class SegmentTemperatureCalculator {
-	private static final double mass = 3500;
-	private static final double specificHeat = 490;
+	private static final double massContainer = 3500;
+	private static final double specificHeatSteel = 490;
+	private static final double massAir = 85.829;
+	private static final double specificHeatAir = 1.006;
 	private static final double area = 101.0785;
-	// convection
 	private static final double rhoAir = 1.184;
-	private static final double velocity = 24.5872; // 55 mph
 	private static final double length = 12.192;
 	private static final double muAir = 1.983e5;
-	private static final double reynolds = (rhoAir * velocity * length) / muAir;
 	private static final double lp = 1005.0;
 	private static final double kAir = 0.0257;
 	private static final double pressure = (lp * muAir) / kAir;
-	private static final double nusselt = 0.037 * Math.pow(reynolds, 0.8) * Math.pow(pressure, 1.0/3.0);
-	// conduction
-	final double kw = 16;
-	final double deltaX = 0.00478;
 	
 	@Autowired
 	private PrefixLogger log;
@@ -36,50 +32,57 @@ public class SegmentTemperatureCalculator {
 	Random random = new Random();
 
 	public Temperature computeFinalTemp(Temperature currentTemp, WeatherData weather, long duration) {
-		double finalTemp = currentTemp.getCelcius();
+		double watts = 0;
+		watts += convectionWatts(currentTemp, weather);
+		//watts += conductionWatts(currentTemp, weather);
+		watts += solarRadiationWatts(weather, new Date());
+		watts += rainWatts(weather);
+		
+		double wallTemp = ((watts * duration) / (massContainer * specificHeatSteel)) + currentTemp.getKelvin();
+		
+		double wallsToInteriorWatts = wallsToInteriorWatts(currentTemp, wallTemp, duration);
+		
+		double airTemp = (duration * wallsToInteriorWatts) / (massAir * specificHeatAir);
 
-		finalTemp += convection(currentTemp, weather, duration);
-		
-		//finalTemp += conduction(currentTemp, weather, duration);
-		
-		finalTemp += solarRadiation(weather, duration, new Date());
-		
-		finalTemp += rainEffect(weather, duration);
-		
-		return new Temperature(finalTemp);
+		//return new Temperature(airTemp, TemperatureUnit.Kelvin);
+		return new Temperature(wallTemp, TemperatureUnit.Kelvin);
 	}
 
-	private double convection(Temperature currentTemp, WeatherData weather, long duration) {
+	private double convectionWatts(Temperature currentTemp, WeatherData weather) {
+		final double velocity = 24.5872; // 55 mph
+		final double reynoldsExterior = (rhoAir * velocity * length) / muAir;
+		final double nusseltExterior = 0.037 * Math.pow(reynoldsExterior, 0.8) * Math.pow(pressure, 1.0/3.0);
+		
 		// inputs
 		final double tempOutside = weather.getTemperature().getKelvin();
 		final double tempCurrent = currentTemp.getKelvin();
 		
 		// calcuations
-		final double convectionWatts = nusselt * kAir * area * (tempOutside - tempCurrent);
-		final double convectionDelta = (duration * convectionWatts) / (mass * specificHeat);
+		final double convectionWatts = nusseltExterior * kAir * area * (tempOutside - tempCurrent);
 		
-		return convectionDelta;
+		return convectionWatts;
 	}
 
-	private double conduction(Temperature currentTemp, WeatherData weather, long duration) {
+	private double conductionWatts(Temperature currentTemp, WeatherData weather) {
+		final double kw = 16;
+		final double deltaX = 0.00478;
+		
 		// inputs
 		final double tempOutside = weather.getTemperature().getKelvin();
 		final double tempCurrent = currentTemp.getKelvin();
 		
 		// calcuations
 		final double conductionWatts = (kw * area * (tempOutside - tempCurrent)) / deltaX;
-		final double conductionDelta = (duration * conductionWatts) / (mass * specificHeat);
-
-		log.debug("Conduction delta temp. celcius=" + conductionDelta);
-		return conductionDelta;
+		
+		return conductionWatts;
 	}
 
-	private double solarRadiation(WeatherData weather, long duration, Date date) {
+	private double solarRadiationWatts(WeatherData weather, Date date) {
 		final long sunrise = weather.getSunrise().getTime();
 		final long sunset = weather.getSunset().getTime();
 		final long current = date.getTime();
 		
-		final double solarDelta;
+		final double solarWatts;
 		
 		if (current < sunset && current > sunrise) {
 			// day time
@@ -91,21 +94,28 @@ public class SegmentTemperatureCalculator {
 			
 			// calcuations
 			final double r0 = 990 * Math.sin(solarAngle) - 30;
-			final double solarWatts = r0 * (1 - (0.75 * Math.pow(skyCover, 3.4)));
-			
-			solarDelta = (duration * solarWatts) / (mass * specificHeat);
+			solarWatts = r0 * (1 - (0.75 * Math.pow(skyCover, 3.4)));
 		} else {
 			// night time
 			// TODO: outgoing radiation effect
-			solarDelta = 0;
+			solarWatts = 0;
 		}
 		
-		return solarDelta;
+		return solarWatts;
 	}
 	
-	private double rainEffect(WeatherData weather, long duration) {
+	private double rainWatts(WeatherData weather) {
 		// TODO: rain effects
 		return 0;
+	}
+
+	private double wallsToInteriorWatts(Temperature currentTemp, double wallTemp, double duration) {
+		final double reynoldsInterior = (rhoAir * length) / muAir;
+		final double nusseltInterior = 0.037 * Math.pow(reynoldsInterior, 0.5) * Math.pow(pressure, 1.0/3.0);
+		
+		double wallsToInteriorWatts = nusseltInterior * kAir / length * area * (wallTemp - currentTemp.getKelvin());
+		
+		return wallsToInteriorWatts;
 	}
 
 }
